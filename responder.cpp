06371784905f2c721d09be6e77d2453c534f7166
wanghou92@gmail.com <wanghou92@gmail.com>
2018-05-02 00:58:54 -0700

    5

--- responder.cpp
+++ responder.cpp
@@ -94,8 +94,8 @@ int Responder::verifyReq(HttpInstruction req){
   /*
     Check and set file extension
   */
-	// cerr << "before checking ext" << '\n';
-  int ext_stat = setFileType(req.url);
+	cerr << "before checking ext" << '\n';
+  // int ext_stat = setFileType(req.url);
   cerr << "set file type" << ext_stat << '\n';
   if(ext_stat != 0){
     return ext_stat;
